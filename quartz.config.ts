cc262e6221b07d526a7b9a660615953dac0c5b08
rekceq <rekceq@tutamail.com>
2026-05-22 22:43:38 -0300

    Quartz sync: May 22, 2026, 10:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Perdigão José Manelson",
+    pageTitle: "Pai do Tiaguinho",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
