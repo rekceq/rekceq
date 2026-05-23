0bdfc9648994d2c3b8ca11b6da12211f11cf49b7
rekceq <rekceq@tutamail.com>
2026-05-22 22:32:22 -0300

    Quartz sync: May 22, 2026, 10:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Bernardo Becker",
+    pageTitleSuffix: "Gamedev Generalist",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
