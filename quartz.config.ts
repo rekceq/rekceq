77de1c8fd01e5da97f0b694df8095186750b0610
rekceq <rekceq@tutamail.com>
2026-05-27 16:23:19 -0300

    Quartz sync: May 27, 2026, 4:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://rekceq.github.io/rekceq/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
