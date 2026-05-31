19a11ee67b0f6d1b967f4c2e78318b49ec807c8c
rekceq <rekceq@tutamail.com>
2026-05-31 03:18:05 -0300

    Quartz sync: May 31, 2026, 3:18 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,12 @@ export const defaultContentPageLayout: PageLayout = {
         //{ Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      filterFn: (node) => {
+      // exclude files with the tag "explorerexclude"
+      return node.data?.tags?.includes("ignore") !== true
+      },
+    })
   ],
   right: [
     Component.Graph(),
